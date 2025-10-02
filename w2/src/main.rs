19bb949f5defc06ed8a887a4aed414caa41ef810
cyprich <cypooriginal@gmail.com>
2025-10-02 21:27:54 +0200

    forgor

--- w2/src/main.rs
+++ w2/src/main.rs
@@ -1,29 +1,90 @@
+use std::io::{Write, stdin, stdout};
+
 use chrono::{Duration, Local, NaiveDate, TimeDelta};
 
 #[derive(Debug)]
 struct Task {
-    id: u32,
+    id: i32,
     name: String,
     description: String,
-    priority: u32,
+    priority: i32,
     planned_from: NaiveDate,
     planned_duration: TimeDelta,
-    real_from: NaiveDate,
-    real_duration: TimeDelta,
+    real_from: Option<NaiveDate>,
+    real_duration: Option<TimeDelta>,
     // dependency: ,
 }
 
-fn main() {
-    let t = Task {
-        id: 0,
-        name: String::from("Create first instance"),
-        description: String::from("Create first instance of Task struct"),
-        priority: 0,
-        planned_from: Local::now().date_naive(),
-        planned_duration: Duration::days(1),
-        real_from: NaiveDate::from_ymd_opt(2025, 10, 2).unwrap_or_default(),
-        real_duration: Duration::days(7),
-    };
+impl Task {
+    fn calculate_planned_end(&self) -> NaiveDate {
+        self.planned_from + self.planned_duration
+    }
 
-    println!("{t:#?}");
+    fn calculate_real_end(&self) -> Option<NaiveDate> {
+        match self.real_from {
+            Some(from) => match self.real_duration {
+                Some(dur) => Option::from(from + dur),
+                None => None,
+            },
+            None => None,
+        }
+    }
+}
+
+fn read_string(message: &str) -> String {
+    print!("{message}");
+    stdout().flush().unwrap();
+    let mut buf = String::new();
+    stdin()
+        .read_line(&mut buf)
+        .expect("Cannot read from console");
+
+    buf.trim().to_string()
+}
+
+fn read_number(message: &str) -> i32 {
+    let result: i32 = read_string(message)
+        .trim()
+        .parse()
+        .expect("Couldn't convert to i32");
+    result
+}
+
+fn read_date(message: &str) -> NaiveDate {
+    NaiveDate::parse_from_str(read_string(message).as_str(), "%d.%m.%Y")
+        .expect("Couldn't parse date")
+}
+
+fn read_timedelta(message: &str) -> TimeDelta {
+    TimeDelta::days(read_number(message).into())
+}
+
+fn create_task_from_console() -> Task {
+    let id = read_number("  ID of Task: ");
+    let name = read_string("  Name of Task: ");
+    let description = read_string("  Description of Task: ");
+    let priority = read_number("  Priority of Task: ");
+    let planned_from = read_date("  Date from: ");
+    let planned_duration = read_timedelta("  Planned duration: ");
+    let real_from = None;
+    let real_duration = None;
+
+    Task {
+        id,
+        name,
+        description,
+        priority,
+        planned_from,
+        planned_duration,
+        real_from,
+        real_duration,
+    }
+}
+
+fn main() {
+    let t = create_task_from_console();
+    let planned_end = t.calculate_planned_end();
+    let real_end = t.calculate_real_end();
+
+    println!("{t:#?}\n{planned_end:?}\n{real_end:?}");
 }
